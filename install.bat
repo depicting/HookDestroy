c67af8457c911e001e74ebb3896a2227cf76b359
ledger <139970644+ledged@users.noreply.github.com>
2023-07-19 08:35:40 -0500

    added installer / display images

---
+++ install.bat
@@ -0,0 +1,6 @@
+pip install pystyle
+pip install colorama
+@echo off
+title HookDestroyer - v1.0
+python hookdestroy.py
+pause
---
+++ open.bat
@@ -0,0 +1,4 @@
+@echo off
+title HookDestroyer - v1.0
+python hookdestroy.py
+pause
